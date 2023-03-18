c95bbf3270ec0a19038174407d123cf17bed13d8
Mayron <mjr_4077@hotmail.co.uk>
2023-03-18 20:18:00 +0000

    Fixed and worked on notifying changes

---
+++ MUI_Core/Libs/OrbitusDB/OrbitusDB.test.lua
@@ -0,0 +1,12 @@
+local api = require("OrbitusDB");
+
+
+---@type DatabaseConfig
+local databaseConfig = {
+
+}
+
+-- api:Register("TestDB", databaseConfig, function(db)
+--   db.profile:
+
+-- end);
